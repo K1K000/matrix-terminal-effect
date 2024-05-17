8826555f761c41698dce4c6f26ffa54e00202ea8
K1K000 <K1K000@users.noreply.github.com>
2024-05-17 16:17:25 +0200

    Update Program.cs

--- matrix-effect/Program.cs
+++ matrix-effect/Program.cs
@@ -1,2 +1,16 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+
+// int height = Console.BufferHeight;
+int height = 40;
+int width = Console.BufferWidth;
+int tail = 7;
+int gencut = 10;
+int amount = 10;
+List<string> characters = ["1", "2", "3", "4",];
+List<string> colors = ["G", "B", "R", "P", "Y",];
+
+
+Dictionary&lt;int, (int, int, char)&gt; myDictionary = new Dictionary&lt;int, (int, int, char)&gt;{};
+
+// myDictionary.Add(1, (10, 20, 'A'));
+
