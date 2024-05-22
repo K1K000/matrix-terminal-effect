bb6d39e128ab5ee17b464a12086c137ece32f533
K1K000 <K1K000@users.noreply.github.com>
2024-05-22 17:54:37 +0200

    what was i even doing omg

--- matrix-effect/Program.cs
+++ matrix-effect/Program.cs
@@ -1,11 +1,11 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
-int height = 20;
+int height = 29;
 // int height = ;
 int width = Console.BufferWidth;
 int tail = 7;
 // int gencut = 10;
-int amount = 5;
+int amount = 10;
 List<string> characters = ["1", "2", "3", "4",];
 List<string> colors = ["G", "B", "R", "P", "Y",];
 System.Console.WriteLine(width);
@@ -20,7 +20,7 @@ string[] alt_c = colors.ToArray(); // colors to array for easy lenght
 
 for (int i = 0; i < amount; i++) // the starting amount of snakes
 {
-    int y = rnd.Next(0,height);
+    int y = rnd.Next(0,height-tail);
     int x = rnd.Next(0,width);
     int c = rnd.Next(0,alt_c.Length-1);
     char character = colors[c][0];
@@ -29,95 +29,52 @@ for (int i = 0; i < amount; i++) // the starting amount of snakes
 
 System.Console.CursorVisible = false;
 System.Console.Clear();
-for (int i = 0; i < 4000; i++)
+for (int i = 0; i < 40000; i++)
 {
     foreach (var ite in table.ToArray())
     {       
         int y = ite.Value.Item2;
         int x = ite.Value.Item1;
         char c = ite.Value.Item3;
-        
-        // // switch (ite.Value.Item2 > height-1 && ite.Value.Item3 != 'E')
-        // // {
-        // //     case true:
-        // //         int yg = rnd.Next(0,height);
-        // //         int xg = rnd.Next(0,width);
-        // //         int cg = rnd.Next(0,alt_c.Length-1);
-        // //         char character = colors[cg][0];
-        // //         table.Remove(ite.Key);
-        // //         table.Add(ite.Key,(xg,yg,character));
-        // //         break;
-        // //     default:
-        // //         break;
-        // }
-
-        if(ite.Value.Item2 > height-1 && ite.Value.Item3 != 'E')
+        if (y >= tail && y <= height)
         {
-            int yg = rnd.Next(0,height);
+            Console.SetCursorPosition(x, y-tail);
+            System.Console.Write(" ");
+            Console.SetCursorPosition(x, y);
+            System.Console.Write(c);
+            table.Remove(ite.Key);
+            y++;
+            table.Add(ite.Key,(x,y,c));
+        }
+        else if (y < tail)
+        {
+            Console.SetCursorPosition(x, y);
+            System.Console.Write(c);
+            table.Remove(ite.Key);
+            y++;
+            table.Add(ite.Key,(x,y,c));
+        }
+        else if (y > height && y < height + tail)
+        {
+            Console.SetCursorPosition(x, y-tail);
+            System.Console.Write(" ");
+            table.Remove(ite.Key);
+            y++;
+            table.Add(ite.Key,(x,y,c));
+        }
+        else if(y >= height + tail)
+        {
+            Console.SetCursorPosition(x, y-tail);
+            System.Console.Write(" ");
+            int yg = rnd.Next(0,height-tail);
             int xg = rnd.Next(0,width);
             int cg = rnd.Next(0,alt_c.Length-1);
             char character = colors[cg][0];
             table.Remove(ite.Key);
             table.Add(ite.Key,(xg,yg,character));
         }
-        switch (ite.Value.Item3 == 'E')
-        {
-            case true:
-                Console.SetCursorPosition(x, y); // bugged for E becomes - idk why
-                System.Console.Write(' ');
-                if (y < height)
-                {
-                    table.Remove(ite.Key);
-                    y++;
-                    table.Add(ite.Key,(x,y,c));
-                }
-                break;
-            case false:
-                Console.SetCursorPosition(x, y);
-                System.Console.Write(c);
-                if (y < height)
-                {
-                    table.Remove(ite.Key);
-                    y++;
-                    table.Add(ite.Key,(x,y,c));
-                }
-                if (y > tail-1)
-                {
-                    Console.SetCursorPosition(x, y-tail);
-                    System.Console.Write(" ");
-                    
-                }
-                if (y > tail && table.ContainsKey(ite.Key+amount))
-                {
-                    table.Remove(ite.Key+amount);
-                    y = y - tail;
-                    table.Add(ite.Key+amount,(x,y,'E'));
-                }
-                break;
-            // case true:
-            //     Console.SetCursorPosition(x, y);
-            //     System.Console.Write(' ');
-            //     if (y < height)
-            //     {
-            //         table.Remove(ite.Key);
-            //         y++;
-            //         table.Add(ite.Key,(x,y,c));
-            //     }
-            //     break;
-            // case 'E':
-            //     Console.SetCursorPosition(x, y);
-            //     System.Console.Write(' ');
-            //     if (y < height)
-            //     {
-            //         table.Remove(ite.Key);
-            //         y++;
-            //         table.Add(ite.Key,(x,y,c));
-            //     }
-            //     break;
-        }
-        
     }
+    System.Threading.Thread.Sleep(100);
 
-    System.Threading.Thread.Sleep(40);
 }
 System.Console.WriteLine("you done goofed");
--- matrix-effect/obj/Debug/net8.0/matrix-effect.AssemblyInfo.cs
+++ matrix-effect/obj/Debug/net8.0/matrix-effect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("matrix-effect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+cfcc09a2a3ce52eaba38abbae628c5488295fe22")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f148fec983bd39b1ac9b7e4c98161dae6e2d837f")]
 [assembly: System.Reflection.AssemblyProductAttribute("matrix-effect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("matrix-effect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
