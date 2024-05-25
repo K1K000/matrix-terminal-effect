522a56c277618a80d32593028be5d7bc66a03f6f
K1K000 <K1K000@users.noreply.github.com>
2024-05-25 22:20:59 +0200

    new rel

--- matrix-effect/obj/Debug/net8.0/matrix-effect.AssemblyInfo.cs
+++ matrix-effect/obj/Debug/net8.0/matrix-effect.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("matrix-effect")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+0c22e8f5ab1a2ba3fce86c515f9a9860f50d9c75")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+bfe39bff640e412efde5c8fbb35f9ab3e9a168ba")]
 [assembly: System.Reflection.AssemblyProductAttribute("matrix-effect")]
 [assembly: System.Reflection.AssemblyTitleAttribute("matrix-effect")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
